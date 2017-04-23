26e8505c34713040740c30a0a5942bb8b6b45078
Marty Boggs <m@thriveline.com>
2017-04-22 19:14:56 -0700

    inside handler

--- js/examples/ex11.js
+++ js/examples/ex11.js
@@ -93,20 +93,13 @@ function setupVR() {
 	};
 	vrButton = new webvrui.EnterVRButton(renderer.domElement, uiOptions);
 
-	// vrButton.domElement.addEventListener('click', bluetoothHandler);
-	// vrButton.domElement.addEventListener('touchend', bluetoothHandler);
+	vrButton.domElement.addEventListener('click', bluetoothHandler);
+	vrButton.domElement.addEventListener('touchend', bluetoothHandler);
 	// window.addEventListener("gamepadconnected", function(e) {
 	// 	console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
 	// 	e.gamepad.index, e.gamepad.id,
 	// 	e.gamepad.buttons.length, e.gamepad.axes.length);
 	// });
-
-	var daydreamController;
-	if (navigator.bluetooth) {
-		daydreamController = new DaydreamController().connect();
-	} else {
-		alert('no navigator.bluetooth');
-	}
 
 	vrButton.on('enter', function () {
 		if (vrButton.state === 'presenting') {
@@ -132,13 +125,13 @@ function setupVR() {
 	setupStage();
 }
 
-// function bluetoothHandler(e) {
-// 	navigator.bluetooth.requestDevice({filters: [
-// 		{services: ['indoor_positioning', 'device_information']}
-// 	]}).then(function (d, e) {
-// 		console.dir(d, e);
-// 	});
-// }
+function bluetoothHandler(e) {
+	if (navigator.bluetooth) {
+		daydreamController = new DaydreamController().connect();
+	} else {
+		alert('no navigator.bluetooth');
+	}
+}
 
 // Get the HMD, and if we're dealing with something that specifies
 // stageParameters, rearrange the scene.
@@ -1442,6 +1435,7 @@ var outsideLight;
 var labLight;
 var kOnce = true;
 var layMeter = 0;
+var daydreamController;
 
 var birdAction = 1;
 var WALKING = 0;
