51d6dde32cc95b69a5a2146bbf029c181b42aaba
Marty Boggs <m@thriveline.com>
2017-04-22 23:39:23 -0700

    what

--- js/examples/ex11.js
+++ js/examples/ex11.js
@@ -935,8 +935,8 @@ function render(timestamp) {
 	// birdY = camera.rotation.y;
 	birdY = Math.atan2(camera.position.z - bird.position.z, bird.position.x - camera.position.x) - Math.PI / 2;
 	if (daydreamState.isClickDown) {
-		var daydreamAngle = Math.atan2(daydreamState.yTouch - 0.5, daydreamState.xTouch - 0.5) - Math.PI;
-		console.log(daydreamAngle);
+		var daydreamAngle = Math.atan2(daydreamState.yTouch - 0.5, daydreamState.xTouch - 0.5);
+		console.log(daydreamAngle * 180 / Math.PI);
 		bodies[0].applyImpulse(bodies[0].getPosition(), new OIMO.Vec3(-500 * Math.sin(birdY + daydreamAngle), 0, -500 * Math.cos(birdY + daydreamAngle)));
 	} else {
 		if (keyboard.pressed('w') || keyboard.pressed('up') || fmb.clicking.UP) {
