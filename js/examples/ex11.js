f97f14bb4d2ff15fbb63cf387e3c8fa278e8d095
Marty Boggs <m@thriveline.com>
2017-04-22 19:11:47 -0700

    mrdoob to the rescue

--- js/examples/ex11.js
+++ js/examples/ex11.js
@@ -95,11 +95,18 @@ function setupVR() {
 
 	// vrButton.domElement.addEventListener('click', bluetoothHandler);
 	// vrButton.domElement.addEventListener('touchend', bluetoothHandler);
-	window.addEventListener("gamepadconnected", function(e) {
-		console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
-		e.gamepad.index, e.gamepad.id,
-		e.gamepad.buttons.length, e.gamepad.axes.length);
-	});
+	// window.addEventListener("gamepadconnected", function(e) {
+	// 	console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+	// 	e.gamepad.index, e.gamepad.id,
+	// 	e.gamepad.buttons.length, e.gamepad.axes.length);
+	// });
+
+	var daydreamController;
+	if (navigator.bluetooth) {
+		daydreamController = new DaydreamController().connect();
+	} else {
+		alert('no navigator.bluetooth');
+	}
 
 	vrButton.on('enter', function () {
 		if (vrButton.state === 'presenting') {
